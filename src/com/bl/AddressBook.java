c9c6415648ccc6121d42becf769ade531d20823e
mohini patange <patangemohini5@gmail.com>
2022-06-08 14:49:27 +0530

    Added : Edit Exiting Contact details

--- src/com/bl/AddressBook.java
+++ src/com/bl/AddressBook.java
@@ -1,27 +1,67 @@
 package com.bl;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class AddressBook {
 
-    public void personInputDetails() {
+    ArrayList<PersonDetails> personDetails = new ArrayList<>();
+
+    public PersonDetails personInputDetails() {
         Scanner sc = new Scanner(System.in);
-        PersonDetails personDetails = new PersonDetails();
+        PersonDetails details = new PersonDetails();
         System.out.println("Enter First Name : ");
-        personDetails.firstName = sc.nextLine();
+        details.firstName = sc.nextLine();
         System.out.println("Enter Last Name : ");
-        personDetails.lastName = sc.nextLine();
+        details.lastName = sc.nextLine();
         System.out.println("Enter Address : ");
-        personDetails.address = sc.nextLine();
+        details.address = sc.nextLine();
         System.out.println("Enter City : ");
-        personDetails.city = sc.nextLine();
+        details.city = sc.nextLine();
         System.out.println("Enter State : ");
-        personDetails.state = sc.nextLine();
+        details.state = sc.nextLine();
         System.out.println("Enter Email : ");
-        personDetails.email = sc.nextLine();
+        details.email = sc.nextLine();
         System.out.println("Enter zip : ");
-        personDetails.zip = sc.nextInt();
+        details.zip = sc.nextInt();
         System.out.println("Enter PhoneNumber : ");
-        personDetails.phoneNumber = sc.nextLong();
+        details.phoneNumber = sc.nextLong();
+        this.personDetails.add(details);
+        return details;
+    }
+
+    public void showDetails() {
+        for (PersonDetails aBook: personDetails) {
+            System.out.println("First name : " + aBook.firstName);
+            System.out.println("last name : " + aBook.lastName);
+            System.out.println("Address : " + aBook.address);
+            System.out.println("City : " + aBook.city);
+            System.out.println("State : " + aBook.state);
+            System.out.println("Email Id : " + aBook.email);
+            System.out.println("Phone number : " + aBook.phoneNumber);
+            System.out.println("Zipcode : " + aBook.zip);
+
+        }
+    }
+    public int findContact(String firstName) {
+        for (PersonDetails cont : personDetails) {
+            if (firstName.compareToIgnoreCase(cont.getFirstName()) == 0) {
+                return personDetails.indexOf(cont);
+            }
+        }
+        return -1;
+    }
+
+    public void editContact() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter first name : ");
+        String firstName = scan.next();
+        int index = findContact(firstName);
+        if (index == 0) {
+            personDetails.set(index, personInputDetails());
+        } else {
+            System.out.println("Contact not found ");
+        }
     }
 }
+
--- src/com/bl/AddressBookMain.java
+++ src/com/bl/AddressBookMain.java
@@ -1,9 +1,45 @@
 package com.bl;
 
+import java.util.Scanner;
+
 public class AddressBookMain {
+
+    static final int ADD_CONTACT = 1;
+    static final int DISPLAY_DETAILS = 2;
+    static final int EDIT_CONTACT = 3;
+    static final int DISPLAY_EDIT_CONTACT = 4;
+
     public static void main(String[] args) {
-        System.out.println("Welcome to Address Book Program!!!");
+        System.out.println("Welcome to Address Book Program!!");
+        Scanner sc = new Scanner(System.in);
         AddressBook addressBook = new AddressBook();
-        addressBook.personInputDetails();
+        int option;
+        do {
+            System.out.println("-------------------------------------------------");
+            System.out.println(" 1.Add a new contact.");
+            System.out.println(" 2.display contact.");
+            System.out.println(" 3.edit contact.");
+            System.out.println(" 4.display edit contact.");
+            option = sc.nextInt();
+
+            switch (option) {
+                case ADD_CONTACT:
+                    addressBook.personInputDetails();
+                    break;
+                case DISPLAY_DETAILS:
+                    addressBook.showDetails();
+                    break;
+                case EDIT_CONTACT:
+                    addressBook.editContact();
+                    break;
+                case DISPLAY_EDIT_CONTACT:
+                    addressBook.showDetails();
+                    break;
+                default:
+                    System.out.println("Enter a valid option");
+                    break;
+            }
+        }
+        while (option != 5);
     }
 }
